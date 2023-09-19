9c4b8bb7897c33472cedde79dd3cdba1502be70a
Jcreasy00 <jcreasy44@gmail.com>
2023-09-19 15:13:24 -0400

    implemented code to change the textView's text size to the number selected in the spinner

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.*
+import android.widget.AdapterView.OnItemSelectedListener
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,7 +26,22 @@ class MainActivity : AppCompatActivity() {
 
 
         // Step 3: Change TextView's text size to the number selected in the Spinner */
-        //spinner.onItemSelectedListener = object: ...
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>, p1: View, p2: Int, id: Long) {
+                p0?.run {
+                    val selectedSize = numberArray[p2]?.toFloat()
+
+                    if (selectedSize != null) {
+                        displayTextView.textSize = selectedSize
+                    }
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // Do nothing here
+                // auto generated
+            }
+        }
 
     }
 }
